627fc02194c4626dad09c4d460854fbd0681ad93
wangyanbing <wangyanbing@lswooks.com>
2023-02-10 11:18:09 +0800

    xx

--- src/main/java/com/eghm/gateway/GatewayApplication.java
+++ src/main/java/com/eghm/gateway/GatewayApplication.java
@@ -1,28 +1,27 @@
 package com.eghm.gateway;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class GatewayApplication {
 
+    @Value("${nacos.name}")
+    private String nacosName;
+
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-    }
-    
-    @Bean
-    public WebFilter webFilter() {
-        return new WebFilter() {
-            @Override
-            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-                return Mono.empty();
-            }
-        };
+        new SpringApplicationBuilder(GatewayApplication.class).web(WebApplicationType.REACTIVE).build().run(args);
     }
 
+    @GetMapping("/nacos/name")
+    public String nacos() {
+        return nacosName;
+    }
 }
